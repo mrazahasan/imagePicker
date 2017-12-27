44a07006f14bd86768aa5bb61edfc08c6490eee1
raza hasan <m_raza_hassan@hotmail.com>
2017-12-28 04:21:31 +0500

    add nativescript mediafilepicker and video support

--- app/item/items.component.ts
+++ app/item/items.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
 import { session, Session } from "nativescript-background-http";
-import * as Picker from "nativescript-imagepicker";
 import * as camera from "nativescript-camera";
 import { first } from 'lodash';
 import {Subject} from "rxjs/Subject";
 import {isAndroid} from "tns-core-modules/platform";
 import * as fs from "file-system";
 import * as imageSource from "image-source";
+import { Mediafilepicker, MediaFilepickerOptions } from 'nativescript-mediafilepicker';
+
 
 @Component({
     selector: "ns-items",
@@ -16,13 +17,58 @@ import * as imageSource from "image-source";
 })
 export class ItemsComponent implements OnInit {
     private UploadSession: Session;
+    private mediafilepicker;
     constructor(
 
     ) {
         this.UploadSession = session('image-upload');
     }
 
-    ngOnInit(): void {
+    ngOnInit() { }
+
+    VideoPicker(){
+        let options: MediaFilepickerOptions = {
+            android: {
+                mxcount: 1,
+                enableImagePicker: false,
+                enableVideoPicker: true,
+                enableCameraSupport: true
+            }
+        };
+        this.mediafilepicker = new Mediafilepicker();
+        this.mediafilepicker.on("getFiles",  (res: any) =>{
+
+            let files = res.files;
+
+            if (files.length > 0) {
+
+                files = files.split(",");
+
+                files.forEach(file => {
+
+                    console.log(file);
+                    this.uploadMultipartImagePicker({fileUri: file})
+                        .subscribe({
+                            next: (e) => {
+                                console.log(`Upload: ${(e.currentBytes / e.totalBytes) * 100}`)
+                            },
+                            error: (e) => {
+                                console.log(JSON.stringify(e));
+                            },
+                            complete: () => {
+                                console.log("complete");
+                            }
+                        });
+                });
+            }else{
+                console.log("There was some problem to select the file. Looks like user has cancel it.")
+            }
+
+        });
+        this.mediafilepicker.on("error",  (res: any) => {
+            console.log(res.msg)
+        });
+        this.mediafilepicker.startFilePicker(options);
     }
 
     cameraOpen(){
@@ -38,7 +84,19 @@ export class ItemsComponent implements OnInit {
                     image = <any>{
                         fileUri: imageAsset.android
                     };
-                    this.uploadImagePicker(image);
+
+                    this.uploadMultipartImagePicker(image)
+                        .subscribe({
+                            next: (e) => {
+                                console.log(`Upload: ${(e.currentBytes / e.totalBytes) * 100}`)
+                            },
+                            error: (e) => {
+                                console.log(JSON.stringify(e));
+                            },
+                            complete: () => {
+                                console.log("complete");
+                            }
+                        });
                 }
                 else {
 
@@ -50,7 +108,18 @@ export class ItemsComponent implements OnInit {
                             let saved = imageSource.saveToFile(path, "png");
                             console.log(saved);
                             if(saved){
-                                this.uploadImagePicker({fileUri: path});
+                                this.uploadMultipartImagePicker({fileUri: path})
+                                    .subscribe({
+                                        next: (e) => {
+                                            console.log(`Upload: ${(e.currentBytes / e.totalBytes) * 100}`)
+                                        },
+                                        error: (e) => {
+                                            console.log(JSON.stringify(e));
+                                        },
+                                        complete: () => {
+                                            console.log("complete");
+                                        }
+                                    });
                             }
                         });
                 }
@@ -61,86 +130,93 @@ export class ItemsComponent implements OnInit {
     }
 
     pickerOpen(){
-        let context = Picker.create(<any>{
-            mode: "single",
-            mdeiaType: 'image'
-        });
-        this.startSelection(context);
-    }
-
-    private startSelection(context: Picker.ImagePicker) {
-        context
-            .authorize()
-            .then(() => {
-                return context.present();
-            })
-            .then((selection: Array<Picker.SelectedAsset>) => {
-                if(isAndroid){
-                    return first(selection);
-                }
-                else {
-                    return first(selection).getImage();
-                }
-
-            })
-            .then((image: any) => {
-                if(isAndroid){
-                    this.uploadImagePicker(image);
-                }
-                else {
-                    let folder = fs.knownFolders.documents();
-                    let path = fs.path.join(folder.path, "Temp" + Date.now() + ".png");
-                    let saved = image.saveToFile(path, "png");
-                    console.log(saved);
-                    if (saved) {
-                        //use new image path
-                        this.uploadImagePicker({fileUri: path});
-                    }
-                }
-            })
-            .catch(function (e) {
-                console.error(e);
-            });
-
-    }
-
-    private uploadImagePicker(image: any) {
-
-        this.uploadMultipartImagePicker(image)
-            .subscribe({
-                next: (e) => {
-                    console.log(`Upload: ${(e.currentBytes / e.totalBytes) * 100}`)
-                },
-                error: (e) => {
-                    console.log(JSON.stringify(e));
-                },
-                complete: () => {
-                    console.log("complete");
-                }
-            });
-    }
-
-
-
-
-
-    uploadMultipartImagePicker(image: any): Subject<any> {
-
-        let  fileUri = image.fileUri;
-
-        let request = {
-            url: "http://35.158.193.55/api/FileUpload/BolSelfie",
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/octet-stream",
-                "File-Name": fileUri,
-                'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiI4MzlmMDg4NC1jMmI3LTQ1ZGUtOTc1Yy0wOTYwMTY4MWE2M2EiLCJ1bmlxdWVfbmFtZSI6IjgzOWYwODg0LWMyYjctNDVkZS05NzVjLTA5NjAxNjgxYTYzYSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYWNjZXNzY29udHJvbHNlcnZpY2UvMjAxMC8wNy9jbGFpbXMvaWRlbnRpdHlwcm92aWRlciI6IkFTUC5ORVQgSWRlbnRpdHkiLCJBc3BOZXQuSWRlbnRpdHkuU2VjdXJpdHlTdGFtcCI6ImFiNGNmOWE2LWVmYzEtNDZhNi05N2YyLWNlNzc5NjkxZGMxMCIsIlVzZXJJZCI6IjgzOWYwODg0LWMyYjctNDVkZS05NzVjLTA5NjAxNjgxYTYzYSIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJhdWQiOiI0MTRlMTkyN2EzODg0ZjY4YWJjNzlmNzI4MzgzN2ZkMSIsImV4cCI6MTU2MzgxMDQ4NiwibmJmIjoxNTExOTcwNDg2fQ.KUdYperMo_79QhR3sU3odmt7LzuCjdSMa25-AU9QLwA",
-                "UserId": "2a2736cb-e5e6-4515-8ad7-02ea0cfef08b"
-            },
-            description: `Uploading `
+        let options: MediaFilepickerOptions = {
+            android: {
+                mxcount: 1,
+                enableImagePicker: true,
+                enableVideoPicker: false,
+                enableCameraSupport: true
+            }
         };
+        this.mediafilepicker = new Mediafilepicker();
+        this.mediafilepicker.on("getFiles",  (res: any) =>{
 
-        let params = [{ name: "nameOfFile", filename: fileUri, mimeType: 'image/png' }];
+            let files = res.files;
+
+            if (files.length > 0) {
+
+                files = files.split(",");
+
+                files.forEach(file => {
+
+                    console.log(file);
+                    this.uploadMultipartImagePicker({fileUri: file})
+                        .subscribe({
+                            next: (e) => {
+                                console.log(`Upload: ${(e.currentBytes / e.totalBytes) * 100}`)
+                            },
+                            error: (e) => {
+                                console.log(JSON.stringify(e));
+                            },
+                            complete: () => {
+                                console.log("complete");
+                            }
+                        });
+                });
+            }else{
+                console.log("There was some problem to select the file. Looks like user has cancel it.")
+            }
+
+        });
+        this.mediafilepicker.on("error",  (res: any) => {
+            console.log(res.msg)
+        });
+        this.mediafilepicker.startFilePicker(options);
+    }
+
+
+    private uploadMultipartImagePicker(image: any): Subject<any> {
+
+        let fileUri = image.fileUri;
+        let filename = fileUri.substring(fileUri.lastIndexOf('/')+1);
+        let mimetype = filename.substring(filename.lastIndexOf('.')+1);
+        let uploadType;
+        let request;
+        if(mimetype === 'mp4'){
+            uploadType = "video";
+            request = {
+                url: "http://httpbin.org/post",
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/octet-stream",
+                    "File-Name": filename,
+                    'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiJiYWM0ZWM2Yi1kNmM4LTQ0NjAtOGU5ZS1mNTUyMWZhZGE3YWUiLCJ1bmlxdWVfbmFtZSI6Im1fcmF6YV9oYXNzYW5AaG90bWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL2FjY2Vzc2NvbnRyb2xzZXJ2aWNlLzIwMTAvMDcvY2xhaW1zL2lkZW50aXR5cHJvdmlkZXIiOiJBU1AuTkVUIElkZW50aXR5IiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiI2Njc3NjE5OS1hYTJjLTQ5YmQtYmY1YS1iMzg2MzY2NTUzOWQiLCJVc2VySWQiOiJiYWM0ZWM2Yi1kNmM4LTQ0NjAtOGU5ZS1mNTUyMWZhZGE3YWUiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiNDE0ZTE5MjdhMzg4NGY2OGFiYzc5ZjcyODM4MzdmZDEiLCJleHAiOjE1NjYyNTQ0MjksIm5iZiI6MTUxNDQxNDQyOX0.fp0IyQSId8sisGQ3kqMPXovzcl8KGZ0M9LBjnSlOpSE",
+                    "UserId": "bac4ec6b-d6c8-4460-8e9e-f5521fada7ae",
+                    "QuestionId": '5a4420b53a5d5a1c38e34028',
+                    "GameId": '13'
+                },
+                description: `Uploading ${filename}`
+            };
+        }
+        else {
+            uploadType = "image";
+            request = {
+                url: "http://httpbin.org/post",
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/octet-stream",
+                    "File-Name": filename,
+                    'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1laWQiOiJiYWM0ZWM2Yi1kNmM4LTQ0NjAtOGU5ZS1mNTUyMWZhZGE3YWUiLCJ1bmlxdWVfbmFtZSI6Im1fcmF6YV9oYXNzYW5AaG90bWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL2FjY2Vzc2NvbnRyb2xzZXJ2aWNlLzIwMTAvMDcvY2xhaW1zL2lkZW50aXR5cHJvdmlkZXIiOiJBU1AuTkVUIElkZW50aXR5IiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiI2Njc3NjE5OS1hYTJjLTQ5YmQtYmY1YS1iMzg2MzY2NTUzOWQiLCJVc2VySWQiOiJiYWM0ZWM2Yi1kNmM4LTQ0NjAtOGU5ZS1mNTUyMWZhZGE3YWUiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiNDE0ZTE5MjdhMzg4NGY2OGFiYzc5ZjcyODM4MzdmZDEiLCJleHAiOjE1NjYyNTQ0MjksIm5iZiI6MTUxNDQxNDQyOX0.fp0IyQSId8sisGQ3kqMPXovzcl8KGZ0M9LBjnSlOpSE",
+                    "UserId": "bac4ec6b-d6c8-4460-8e9e-f5521fada7ae",
+                    "QuestionId": '5a4420b53a5d5a1c38e34028',
+                    "GameId": '2'
+                },
+                description: `Uploading `
+            };
+        }
+
+
+        let params = [{ name: filename, filename: fileUri, mimeType: `${uploadType}/${mimetype}` }];
         console.log(JSON.stringify(params));
 
         let subject = new Subject<any>();
